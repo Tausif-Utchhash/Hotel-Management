aed3d295e9becdf6f601a8049851ed00ff08b483
tausif utchhash <tausif.tazwar.quadria.utchhash@g.bracu.ac.bd>
2022-08-18 19:47:18 +0600

    first commit

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,7 @@
+@extends('frontlayout')
+@section('content')
+<div class="container my-4">
+	<h3 class="mb-3">About Us</h3>
+	<p>The DBZ chain began with two motels in the 2010s. The first opened as a Quality Inn airport motel near Banani, DHAKA. and another motel nearby, the DB-KAI and DB-Super, a few years later. With the opening of the second motel, DBZ was born as a brand name. The DB-KAI and DB-Super property was demolished in 2014, but the DB-KAI property still operates, but as a full-service hotel. In 2015, DBZ opened its first resort hotel, DB-Zeno, in Uttora, Dhaka. DBZ Hotels & Resorts expanded outside of Dhaka for the first time in 1916 with the opening of the DB-Vados in Rangpur, Bangladesh. By 2017, DBZ Hotels & Resorts had expanded to Rajshahi, with the DB-Weise opening that year. By 2022, there were over 50 DBZ Hotels & Resorts in 47 Districts, and in August 2022, DBZ Hotels & Resorts announced the opening of what is said was their 100th property. In July 2022, DBZ Hotels & Resorts unveiled its first new room designs in ten years. Dubbed "mSpot," the new rooms feature clean lines and updated technology.</p>
+</div>
+@endsection
